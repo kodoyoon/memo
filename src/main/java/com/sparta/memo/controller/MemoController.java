d2ac5a06f4924ef989f8ce6958196f15cd658a7f
doyoon <doyoun0527@naver.com>
2023-11-01 17:34:24 +0900

    Update, Delete 구현

---
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -0,0 +1,77 @@
+package com.sparta.memo.controller;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+  private final Map<Long, Memo> memoList = new HashMap<>();
+  @PostMapping("/memos")
+  public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) { //요청은  BODY
+   //Request -> Entity
+    Memo memo = new Memo(requestDto);
+
+      //Memo Max ID Check
+    Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+    memo.setId(maxId);
+    //DB 저장
+    memoList.put(memo.getId(), memo);
+
+    //Entity -> ResponseDto
+    MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+
+    return memoResponseDto;
+  }
+
+  @GetMapping("/memos")
+  public List<MemoResponseDto> getMemos() {
+
+    // Map To List
+    List<MemoResponseDto> responseList = memoList.values().stream()
+        .map(MemoResponseDto::new).toList();
+
+    return responseList;
+
+
+  }
+
+  @PutMapping("/memos/{id}")
+  public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {              // json 은 RequestBody
+    //해당 메모가 DB 에 존재 하는지 확인
+    if(memoList.containsKey(id)) {
+      //해당 메모를 가져오기
+      Memo memo = memoList.get(id);
+
+      // memo 수정
+      memo.update(requestDto);
+      return memo.getId();
+    } else {
+      throw new IllegalArgumentException("선택한 메뉴는 존재하지 않습니다.");
+    }
+  }
+
+  @DeleteMapping("/memos/{id}")
+  public Long deleteMemo(@PathVariable Long id) {
+    //해당 메모가  db 에 존재하는지 확인
+    if(memoList.containsKey(id)) {
+//해당 메모를 삭제하기
+      memoList.remove(id);
+      return id;
+    }else{
+     throw new IllegalArgumentException("선택한 메뉴는 존재하지 않습니다.");
+    }
+
+
+  }
+
+
+
+
+
+}
