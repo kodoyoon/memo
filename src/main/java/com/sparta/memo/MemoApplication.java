e24d014b6c035b4a88bb26d92594a1468020ce56
doyoon <doyoun0527@naver.com>
2023-11-10 16:47:19 +0900

    JPA Auditing 적용하기

--- src/main/java/com/sparta/memo/MemoApplication.java
+++ src/main/java/com/sparta/memo/MemoApplication.java
@@ -2,7 +2,10 @@ package com.sparta.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing //JPA AUDING 기능을 사용하겠따
 @SpringBootApplication
 public class MemoApplication {
 
---
+++ src/main/java/com/sparta/memo/entity/Timestamped.java
@@ -0,0 +1,27 @@
+package com.sparta.memo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+
+  @CreatedDate
+  @Column(updatable = false) //업데이트가 되지 않게끔 막아줌
+  @Temporal(TemporalType.TIMESTAMP)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column
+  @Temporal(TemporalType.TIMESTAMP)
+  private LocalDateTime modifiedAt;
+
+
+}
